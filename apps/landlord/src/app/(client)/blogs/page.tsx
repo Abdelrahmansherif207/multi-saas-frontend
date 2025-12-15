ac2d8acd56afe956a984f669e246c32b91e94e06
K-nass <karimnase10@gmail.com>
2025-12-15 20:27:29 +0200

    temporarily hardcode API URLs to deployed Vercel site

--- apps/landlord/src/app/(client)/blogs/page.tsx
+++ apps/landlord/src/app/(client)/blogs/page.tsx
@@ -14,7 +14,7 @@ export default async function blogsPage({
 }) {
   const { query, category } = await searchParams;
   const res = await fetch(
-    `http://localhost:3000/api/blogs?query=${encodeURIComponent(query ?? "")}&category=${encodeURIComponent(category ?? "")}`
+    `https://multi-saas-frontend-landlord.vercel.app/api/blogs?query=${encodeURIComponent(query ?? "")}&category=${encodeURIComponent(category ?? "")}`
   );
   const blogs: BlogInterface[] = await res.json();
   return (
