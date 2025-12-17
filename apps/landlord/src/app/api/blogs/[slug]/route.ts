028bc111a849fd8935c33922d1859833a2d193f9
K-nass <karimnase10@gmail.com>
2025-12-17 13:10:52 +0200

    fix route.ts

--- apps/landlord/src/app/api/blogs/[slug]/route.ts
+++ apps/landlord/src/app/api/blogs/[slug]/route.ts
@@ -2,9 +2,9 @@ import { blogs } from "@/app/lib/definitions";
 
 export async function GET(
   _request: Request,
-  { params }: { params: { slug: string } }
+  { params }: { params: Promise<{ slug: string }> }
 ) {
-  const { slug } = (await params);  
+  const { slug } = await params;  
   const blog = blogs.find((b) => b.slug === slug);
   if (!blog) {
     return Response.json({ error: "blog not found" }, { status: 404 });
